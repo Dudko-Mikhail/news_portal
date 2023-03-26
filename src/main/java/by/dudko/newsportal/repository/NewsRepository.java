bc46e0589d76639ba08bfb7e63550b6c8b0e0abe
Dudko Mikhail <dudkomikhail01@gmail.com>
2023-03-26 16:03:36 +0300

    refactored news repository

--- src/main/java/by/dudko/newsportal/repository/NewsRepository.java
+++ src/main/java/by/dudko/newsportal/repository/NewsRepository.java
@@ -4,11 +4,14 @@ import by.dudko.newsportal.model.News;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
     Page<News> findAllByOwnerId(long userId, Pageable pageable);
 
-    List<Long> findAllNewsIdByOwnerId(long userId);
+    @Query("select n.id from News n where n.ownerId = :ownerId")
+    List<Long> findAllNewsIdByOwnerId(@Param(value = "ownerId") long userId);
 }
